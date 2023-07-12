d616cc0c10d6394c9722e1438f3add5c91788b91
Dennis <dccy1022@outlook.com>
2023-07-12 16:00:29 +0800

    change for auth user

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -1,9 +1,31 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator')
 
 var userschema= new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
+    admin: {
+      type: Boolean,
+      default: false,
+      },
+    retailer: {
+      type: Boolean,
+      default: false,
+      },
+    name: {
+        type: String,
+        required: [true, "Please add the contact name"],
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        validate: [isEmail, "Please enter a valid email"]
+        },
+      password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 8,
+      },
 })
 
 
